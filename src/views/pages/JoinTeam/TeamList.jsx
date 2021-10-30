e49e25f94b6dafe2df0d9cc47d4c41ac9b10f7bd
lovish tater <lovishtater@gmail.com>
2021-10-30 22:57:14 +0530

    Minor experiments

--- src/views/pages/JoinTeam/TeamList.jsx
+++ src/views/pages/JoinTeam/TeamList.jsx
@@ -1,14 +1,14 @@
 import PlayerCard from "./PlayerCard";
 
 function TeamList(props) {
-  const { players, onSelectPlayer, teamName } = props;
+  const { players, onSelectPlayer, teamName, displayPicture } = props;
   return (
     <>
       <h1>{teamName}</h1>
       <div style={ListContainerStyle}>
         {players?.map((player) => {
           return (
-            <PlayerCard playerInfo={player} onSelectPlayer={onSelectPlayer} />
+            <PlayerCard  playerInfo={player} onSelectPlayer={onSelectPlayer} />
           );
         })}
       </div>
