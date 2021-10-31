6b53ceaa0d9fa1c387188fe248860f0ab618ec83
hardikhere <web.hardikhere@gmail.com>
2021-10-31 08:24:46 +0530

    remove player functionality

--- src/utils/data.js
+++ src/utils/data.js
@@ -7,48 +7,62 @@ export const MATCHES = [
       teamName: "England",
       players: [
         {
-          playerName: "Virat",
-          displayPicture: "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
+          playerName: "Morgan",
+          displayPicture:
+            "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
           pointRequired: 9,
           playerType: "Batsman",
+          playerId: "Morgan1",
         },
         {
-          playerName: "Dhoni",
-          displayPicture: "https://cdn.britannica.com/25/222725-050-170F622A/Indian-cricketer-Mahendra-Singh-Dhoni-2011.jpg",
+          playerName: "Ali",
+          displayPicture:
+            "https://cdn.britannica.com/25/222725-050-170F622A/Indian-cricketer-Mahendra-Singh-Dhoni-2011.jpg",
           pointRequired: 9,
+          playerId: "Ali2",
           playerType: "Batsman",
         },
         {
-          playerName: "Rohit",
-          displayPicture: "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
-          pointRequired:11,
-          playerType: "Batsman",
-        },
-        {
-          playerName: "Shikhar",
-          displayPicture: "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
+          playerName: "Stokes",
+          playerId: "Stokes3",
+          displayPicture:
+            "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
           pointRequired: 11,
           playerType: "Batsman",
         },
         {
-          playerName: "Rohit",
-          displayPicture: "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
+          playerName: "Archer",
+          playerId: "Arc324her",
+          displayPicture:
+            "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
           pointRequired: 11,
           playerType: "Batsman",
         },
         {
-          playerName: "Hardik Pandya",
-          displayPicture: "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
+          playerName: "Bairstow",
+          playerId: "Bairstow2341",
+          displayPicture:
+            "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
           pointRequired: 11,
           playerType: "Batsman",
         },
         {
-          playerName: "Umesh Yadav",
+          playerName: "Billings Pandya",
+          playerId: "Billings234",
+          displayPicture:
+            "https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg",
+          pointRequired: 11,
+          playerType: "Batsman",
+        },
+        {
+          playerName: "Buttler Yadav",
           pointRequired: 12,
+          playerId: "Buttler23423499",
           playerType: "Bowler",
         },
         {
-          playerName: "Jasprit Bumrah",
+          playerName: "Jasprit Curran",
+          playerId: "Curran923847",
           pointRequired: 8,
           playerType: "Bowler",
         },
@@ -61,46 +75,55 @@ export const MATCHES = [
           playerName: "Hardik Pandya",
           pointRequired: 9,
           playerType: "Batsman",
+          playerId: "hardik_93874",
         },
-        
+
         {
           playerName: "Virat",
+          pointRequired: 9,
+          playerId: "virat_234876",
+          playerType: "Batsman",
+        },
+        {
+          playerName: "Dhoni",
+          playerId: "dhoni_987",
           pointRequired: 9,
           playerType: "Batsman",
         },
         {
-          playerName: "Virat",
+          playerName: "Rohit Sharma",
+          playerId: "rohit_897384",
           pointRequired: 9,
           playerType: "Batsman",
         },
         {
-          playerName: "Virat",
-          pointRequired: 9,
-          playerType: "Batsman",
-        },
-        {
-          playerName: "Virat",
+          playerName: "KL Rahul",
+          playerId: "KL_098234",
           pointRequired: 9,
           playerType: "Baller",
         },
         {
-          playerName: "Virat",
+          playerName: "Rishabh Pant",
+          playerId: "pant_0938248",
           pointRequired: 9,
           playerType: "Baller",
         },
         {
-          playerName: "Virat",
+          playerName: "Mohammad Shami",
+          playerId: "shami_09238444",
           pointRequired: 9,
           playerType: "Baller",
         },
         {
-          playerName: "Virat",
+          playerName: "Ravindra Jadeja",
           pointRequired: 9,
+          playerId: "jadeja_023847",
           playerType: "Baller",
         },
         {
-          playerName: "Virat",
+          playerName: "Shardul Thakur",
           pointRequired: 9,
+          playerId: "thakur_93982374",
           playerType: "Baller",
         },
       ],
@@ -113,7 +136,7 @@ export const MATCHES = [
   },
 ];
 
-const match_info = {
+export const match_info = {
   virat: {
     wicket: 0,
     runs: 0,
