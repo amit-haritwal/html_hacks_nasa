82c8ea7788dffba3c4d939a10201a978d67d4e48
amit-haritwal <69576203+amitintern@users.noreply.github.com>
2021-10-31 15:27:48 +0530

    temnp

---
+++ src/views/pages/Leaderboard/LeaderBoard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LeaderBoard() {
+	return (
+		<div>
+			<h1>sdfds</h1>
+		</div>
+	);
+}
+
+export default LeaderBoard;
---
+++ src/views/pages/Leaderboard/UserCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function userCard() {
+	return (
+		<div>
+			<h1>user card</h1>
+		</div>
+	);
+}
+
+export default userCard;
